16dabf1fb6a0cfbfab10d80b71d414e13e8f8ce7
GONZALO LEONEL TUJCHER <43409196@ort>
2018-10-16 09:50:56 -0300

    backoffice 1

---
+++ QEQ/QEQ/Controllers/BOmenuController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BOmenuController : Controller
+    {
+        // GET: BOmenu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ABMCarac()
+        {
+            return ABMCarac();
+        }
+        public ActionResult ABMCateg()
+        {
+            return ABMCateg();
+        }
+        public ActionResult ABMPers()
+        {
+            return ABMPers();
+        }
+
+    }
+}
